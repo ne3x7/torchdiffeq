Bug with gradients found

torchdiffeq/_impl/adjoint.py
@@ -39,11 +39,13 @@ class OdeintAdjointMethod(torch.autograd.Function):
                 t = t.to(y[0].device).detach().requires_grad_(True)
                 y = tuple(y_.detach().requires_grad_(True) for y_ in y)
                 func_eval = func(t, y, exog_y)
-                print('t', t, 'y', y, 'exog', exog_y)
-                print('eval', func_eval)
 
-                assert None is not None
-                
+                if t.item() < 1:
+                    print('t', t, 'y', y, 'exog', exog_y)
+                    print('eval', func_eval)
+
+                    assert None is not None
+
                 vjp_t, *vjp_y_and_params = torch.autograd.grad(
                     func_eval, (t,) + y + f_params,
                     tuple(-adj_y_ for adj_y_ in adj_y), allow_unused=True, retain_graph=True