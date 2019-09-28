6f7c9e6ece2dd224dc883859bd1d32d795a9dfce
ne3x7 <nickstulov@gmail.com>
2019-09-28 20:11:15 +0300

    Fix constraints bug

--- torchdiffeq/_impl/solvers.py
+++ torchdiffeq/_impl/solvers.py
@@ -3,6 +3,7 @@ import torch
 from .misc import _assert_increasing, _handle_unused_kwargs
 
 def process(x):
+    print(x.data.shape)
     if len(x.data.shape) > 1:
         x.data[..., 3].clamp_(-0.2, 0.2)
         x.data[..., 6].clamp_(-6.4, 7)
